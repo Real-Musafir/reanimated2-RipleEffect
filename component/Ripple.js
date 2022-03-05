0b6b7ee833eb1de9601badcf3c65e8b26498b13a
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-03-05 15:42:08 +0600

    implement tapGastureHandler

--- component/Ripple.js
+++ component/Ripple.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { View } from "react-native";
+import { TapGestureHandler } from "react-native-gesture-handler";
+import Animated, { useAnimatedGestureHandler } from "react-native-reanimated";
 
 function Ripple({ style, onTap, children }) {
-  return <View style={style}>{children}</View>;
+  const tapGestureEvent = useAnimatedGestureHandler({
+    onStart: () => {},
+    onActive: () => {
+      console.log("tap");
+    },
+    onEnd: () => {},
+  });
+
+  return (
+    <TapGestureHandler onGestureEvent={tapGestureEvent}>
+      <Animated.View>
+        <View style={style}>{children}</View>
+      </Animated.View>
+    </TapGestureHandler>
+  );
 }
 
 export default Ripple;
