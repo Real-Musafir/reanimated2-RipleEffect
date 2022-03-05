82bc5974344172d538087cbf2faf253156e80d45
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-03-05 16:06:45 +0600

    addjust the circle position in ripple Square

--- component/Ripple.js
+++ component/Ripple.js
@@ -4,6 +4,7 @@ import { TapGestureHandler } from "react-native-gesture-handler";
 import Animated, {
   runOnJS,
   useAnimatedGestureHandler,
+  useAnimatedStyle,
 } from "react-native-reanimated";
 
 function Ripple({ style, onTap, children }) {
@@ -15,10 +16,28 @@ function Ripple({ style, onTap, children }) {
     onEnd: () => {},
   });
 
+  const rStyle = useAnimatedStyle(() => {
+    const circleRadius = 200;
+    return {
+      width: circleRadius * 2,
+      height: circleRadius * 2,
+      borderRadius: circleRadius,
+      opacity: 0.2,
+      backgroundColor: "red",
+      position: "absolute",
+      transform: [
+        {
+          scale: 0.5,
+        },
+      ],
+    };
+  });
+
   return (
     <TapGestureHandler onGestureEvent={tapGestureEvent}>
-      <Animated.View>
-        <View style={style}>{children}</View>
+      <Animated.View style={style}>
+        <View>{children}</View>
+        <Animated.View style={rStyle} />
       </Animated.View>
     </TapGestureHandler>
   );
